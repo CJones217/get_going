1cea5e7ebb5eb1f07450d09f4c04e6647f31f097
Collin Jones <cjones28@luc.edu>
2020-05-04 21:28:12 -0500

    initial plan. array of arrays to organize the goals. maybe use a hashmap instead

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+//future additions, pick based on level of urgency (tuple with string and level of urgency)
+
+
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut defcon0_items = ["resume","computer forensics", "internship", "research"];
+    let mut defcon1_items = ["rubicks cube","coding problems", "reverse engineer logicola"];
+    let mut defcon2_items = ["arduino","pokedex", "read","wireshark"];
+
+    let mut list_of_shit = [defcon0_items,defcon1_items,defcon2_items];
+    let mut random_number = rand::thread_rng().gen_range(1, list_of_shit.len());
+
+
+    println!("get off your ass");
+
+
 }
