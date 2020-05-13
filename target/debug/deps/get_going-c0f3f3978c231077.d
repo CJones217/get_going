71e532ecaffa8fa6368968e42bd9d51ceed035af
CJones217 <cjones28@luc.edu>
2020-05-12 20:43:01 -0500

    get manaro working

---
+++ target/debug/deps/get_going-c0f3f3978c231077.d
@@ -0,0 +1,5 @@
+/home/collinj/Code_Projects/get_going/target/debug/deps/get_going-c0f3f3978c231077: src/main.rs
+
+/home/collinj/Code_Projects/get_going/target/debug/deps/get_going-c0f3f3978c231077.d: src/main.rs
+
+src/main.rs:
